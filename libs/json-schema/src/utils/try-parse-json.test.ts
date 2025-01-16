b889b6aeb7dea6eda95ba79a2f9c26788be9d88a
imhonglu <anonymouser.unknown@gmail.com>
2025-01-16 13:25:10 +0900

    feat: add tryParseJson utility

---
+++ libs/json-schema/src/utils/try-parse-json.test.ts
@@ -0,0 +1,37 @@
+import { expect, test } from "vitest";
+import { tryParseJson } from "./try-parse-json.js";
+
+test("should return number as-is", () => {
+	expect(tryParseJson(123)).toBe(123);
+});
+
+test("should return boolean as-is", () => {
+	expect(tryParseJson(true)).toBe(true);
+	expect(tryParseJson(false)).toBe(false);
+});
+
+test("should return null as-is", () => {
+	expect(tryParseJson(null)).toBe(null);
+});
+
+test("should return undefined as-is", () => {
+	expect(tryParseJson(undefined)).toBe(undefined);
+});
+
+test("should return object as-is", () => {
+	const obj = { foo: "bar" };
+	expect(tryParseJson(obj)).toBe(obj);
+});
+
+test("should return array as-is", () => {
+	const arr = [1, 2, 3];
+	expect(tryParseJson(arr)).toBe(arr);
+});
+
+test("should parse JSON object string", () => {
+	expect(tryParseJson('{"foo":"bar"}')).toEqual({ foo: "bar" });
+});
+
+test("should parse JSON array string", () => {
+	expect(tryParseJson("[1,2,3]")).toEqual([1, 2, 3]);
+});
