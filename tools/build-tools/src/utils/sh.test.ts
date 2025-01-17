162511138883f7e4eaa9357e414c440aa344ca10
imhonglu <anonymouser.unknown@gmail.com>
2025-01-17 16:26:52 +0900

    test: skip tests on Windows

--- tools/build-tools/src/utils/sh.test.ts
+++ tools/build-tools/src/utils/sh.test.ts
@@ -1,31 +1,43 @@
 import { expect, test } from "vitest";
 import { isExecException, safeSh, sh } from "./sh.js";
 
-test("sh should execute command and return stdout", async () => {
-  const result = await sh('echo "This is a test"');
-  expect(result.trim()).toBe("This is a test");
-});
+test.skipIf(process.platform === "win32")(
+  "sh should execute command and return stdout",
+  async () => {
+    const result = await sh('echo "This is a test"');
+    expect(result).toBe("This is a test\n");
+  },
+);
 
-test("sh should handle errors correctly", async () => {
-  try {
-    await sh("exit 1");
-  } catch (error) {
-    expect(isExecException(error)).toBe(true);
-    if (isExecException(error)) {
-      expect(error.code).toBe(1);
+test.skipIf(process.platform === "win32")(
+  "sh should handle errors correctly",
+  async () => {
+    try {
+      await sh("exit 1");
+    } catch (error) {
+      expect(isExecException(error)).toBe(true);
+      if (isExecException(error)) {
+        expect(error.code).toBe(1);
+      }
     }
-  }
-});
+  },
+);
 
-test("safeSh should return result if no error", async () => {
-  const result = await safeSh('echo "This is a test"');
+test.skipIf(process.platform === "win32")(
+  "safeSh should return result if no error",
+  async () => {
+    const result = await safeSh('echo "This is a test"');
 
-  expect(result.ok && result.data.trim()).toBe("This is a test");
-});
+    expect(result.ok && result.data).toBe("This is a test\n");
+  },
+);
 
-test("safeSh should return error if error", async () => {
-  const result = await safeSh("exit 1");
+test.skipIf(process.platform === "win32")(
+  "safeSh should return error if error",
+  async () => {
+    const result = await safeSh("exit 1");
 
-  expect(!result.ok && isExecException(result.error)).toBe(true);
-  expect(!result.ok && result.error.code).toBe(1);
-});
+    expect(!result.ok && isExecException(result.error)).toBe(true);
+    expect(!result.ok && result.error.code).toBe(1);
+  },
+);
