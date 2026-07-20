5013edd41c196a549218f00cf3e6f0f4fc00ee67
imhonglu <anonymouser.unknown@gmail.com>
2026-07-20 14:55:10 +0900

    fix: skip cjs conversion without dist

--- tools/build-tools/src/esm-to-cjs.cli.ts
+++ tools/build-tools/src/esm-to-cjs.cli.ts
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
+import { access } from "node:fs/promises";
 import esbuild from "esbuild";
 
 (async () => {
+  try {
+    await access("dist");
+  } catch {
+    return;
+  }
+
   await esbuild.build({
     entryPoints: ["dist/**/*.js"],
     format: "cjs",
